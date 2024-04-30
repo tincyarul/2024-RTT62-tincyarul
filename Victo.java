bc9377c9bad40a9e30a1203052a5d19627b389b3
TincyArul <arultincy@gmail.com>
2024-04-30 17:42:16 -0400

    moving file into repo

---
+++ Victo.java
@@ -0,0 +1,27 @@
+public class Array {
+    public static void main(String[] args) {
+        String[] StudentNames = new String[] {"Sophia","Thomas","Theodore","Nakshatra","Arul","Victoria"};
+        int[] StudentAge = new int[] {6,7,8,9,10};
+        double[] StudentGrade = new double[] {1.5,2.0,3.5,4.0,4.5};
+        String[] StudentSubjects = new String[5];
+        StudentSubjects[0]="English";
+        StudentSubjects[1]="Science";
+        StudentSubjects[2]="Social";
+        StudentSubjects[3]="Tamil";
+        StudentSubjects[4]="Maths";
+        System.out.println(StudentNames[0]);
+        System.out.println(StudentNames[2]);
+        System.out.println(StudentNames[4]);
+        System.out.println(StudentAge[0]);
+        System.out.println(StudentAge[2]);
+        System.out.println(StudentAge[4]);
+        System.out.println(StudentGrade[0]);
+        System.out.println(StudentGrade[2]);
+        System.out.println(StudentGrade[4]);
+        System.out.println(StudentSubjects[0]);
+        System.out.println(StudentSubjects[2]);
+        System.out.println(StudentSubjects[4]);
+
+
+    }
+}
