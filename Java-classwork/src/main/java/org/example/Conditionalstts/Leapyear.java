fce5f78779ef091e9c4e09b08b9a96b2a087e9ba
TincyArul <arultincy@gmail.com>
2024-05-03 13:54:09 -0400

    todays work to repo

--- Java-classwork/src/main/java/org/example/Conditionalstts/Leapyear.java
+++ Java-classwork/src/main/java/org/example/Conditionalstts/Leapyear.java
@@ -4,7 +4,7 @@ public class Leapyear {
     public static void main(String[] args) {
         int year;
         year =2004;
-        if(year % 4 == 0 && year % 4 >= 0) {
+        if(year % 4 == 0 && year % 4 > 0) {
             System.out.println(" It is a leap year");
         }
         else {
--- Java-classwork/src/main/java/org/example/Conditionalstts/casestt.java
+++ Java-classwork/src/main/java/org/example/Conditionalstts/casestt.java
@@ -1,2 +1,39 @@
-package org.example.Conditionalstts;public class casestt {
+package org.example.Conditionalstts;
+
+public class casestt {
+    public static void main(String[] args)
+    {
+        String output;
+        int grade;
+        grade=60;
+        switch(grade/10) {
+            case 10:
+                ;
+                System.out.println("output is" + "Excellent");
+                break;
+            case 9:
+                ;
+                System.out.println("output is" + "Wow");
+                break;
+            case 8:
+                ;
+                System.out.println("output is" + "very good");
+                break;
+            case 7:
+                ;
+                System.out.println("output is" + "good");
+                break;
+            case 6:
+                ;
+                System.out.println("output is" + "average");
+                break;
+            default:
+                ;
+                System.out.println("output is" + "fail");
+                break;
+
+        }
+
+
+    }
 }
