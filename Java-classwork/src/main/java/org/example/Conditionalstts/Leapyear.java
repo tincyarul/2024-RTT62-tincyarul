5218a3156ebe913358f0bf7fb41498ff424e15db
TincyArul <arultincy@gmail.com>
2024-05-03 13:18:13 -0400

    Move to repo

---
+++ Java-classwork/src/main/java/org/example/Conditionalstts/Leapyear.java
@@ -0,0 +1,14 @@
+package org.example.Conditionalstts;
+
+public class Leapyear {
+    public static void main(String[] args) {
+        int year;
+        year =2004;
+        if(year % 4 == 0 && year % 4 >= 0) {
+            System.out.println(" It is a leap year");
+        }
+        else {
+            System.out.println("It is not a leap year");
+        }
+    }
+}
