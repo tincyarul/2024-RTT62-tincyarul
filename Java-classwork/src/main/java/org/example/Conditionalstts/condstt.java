4664fb7d4102de0006e61e01d4d624558c526864
TincyArul <arultincy@gmail.com>
2024-05-03 11:52:15 -0400

    move the code to repo

---
+++ Java-classwork/src/main/java/org/example/Conditionalstts/condstt.java
@@ -0,0 +1,17 @@
+package org.example.Conditionalstts;
+
+public class condstt {
+    int x=5;
+    if (x > 5)
+    {
+        System.out.println("Admit to primary school");
+    }
+    elseif (x >10)
+    {
+        System.out.println("Admit to middle school");
+    }
+    else
+    {
+        System.out.println("Do not Admit to school");
+    }
+}
