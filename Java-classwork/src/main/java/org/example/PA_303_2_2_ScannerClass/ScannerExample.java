a79730eff4dc5a7add4b01d40659c6ff7f2533b1
TincyArul <arultincy@gmail.com>
2024-05-03 01:18:18 -0400

    move homework to repo

---
+++ Java-classwork/src/main/java/org/example/PA_303_2_2_ScannerClass/ScannerExample.java
@@ -0,0 +1,23 @@
+package org.example.PA_303_2_2_ScannerClass;
+
+import java.util.Scanner;
+
+public class ScannerExample{
+
+            public static void main(String[] args)
+        {
+            Scanner scan = new Scanner(System.in);
+
+            int i = scan.nextInt();
+            double d = scan.nextDouble();
+            scan.nextLine();
+            String s = scan.nextLine();
+            scan.close();
+
+
+            System.out.println("String: " + s);
+            System.out.println("Double: " + d);
+            System.out.println("Int: " + i);
+        }
+    }
+
--- Java-classwork/src/main/java/org/example/PA_303_2_3_OperatorsAndNumbers/Task2_RightShiftExample.java
+++ Java-classwork/src/main/java/org/example/PA_303_2_3_OperatorsAndNumbers/Task2_RightShiftExample.java
@@ -1,14 +1,6 @@
 package org.example.PA_303_2_3_OperatorsAndNumbers;
 
-//Write a program that declares a variable x, and assigns 150 to it, and prints out the
-//binary string version of the number. Now use the right shift operator (>>) to shift by 2
-//and assign the result to x. Write a comment indicating what you anticipate the
-//decimal and binary values to be. Now print the value of x and the binary string.
-//Perform the preceding exercise with the following values:
-//a. 225
-//b. 1555
-//c. 32456
-
+//
 public class Task2_RightShiftExample {
         public static void main(String[] args) {
             // a. For 225:
